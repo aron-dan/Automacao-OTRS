e0b937f3287dbb8c6af5b91eb6fd2368e17c50ef
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-06-10 00:01:02 -0300

    HotFix: Refeito todo o projeto para conseole aplication ao inver de TesteUnitário

---
+++ ConsoleApp1/ConsoleApp1/AutomationWeb.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AutomationWeb
+    {
+        public IWebDriver driver;
+        public AutomationWeb()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public void WebTest()
+        {
+            driver.Navigate().GoToUrl("https://google.com");
+        }
+    
+    
+    }
+}
