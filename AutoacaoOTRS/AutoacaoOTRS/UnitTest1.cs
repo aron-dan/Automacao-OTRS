6ccaae421ddb6e5c10ca3c8501e32ab909f3638a
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-06-09 22:28:29 -0300

    Feat: Criado projeto e importado o selenium WebDrive para inicio da automacao

---
+++ AutoacaoOTRS/AutoacaoOTRS/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutoacaoOTRS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+            driver.Quit();
+        }
+    }
+}
