55ecde38a89196c02daf4a7c1340a2964548b17c
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-06-23 22:24:55 -0300

    Feat: Aparece asterisco ao digitar no campo de senha

--- ConsoleApp1/ConsoleApp1/ColetaUserESenhaForm.Designer.cs
+++ ConsoleApp1/ConsoleApp1/ColetaUserESenhaForm.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(29, 33);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.TabIndex = 0;
+            this.labelUsuario.Text = "Usuario";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(80, 33);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUsuario.TabIndex = 1;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(80, 104);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(80, 59);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSenha.TabIndex = 4;
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Location = new System.Drawing.Point(29, 59);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(38, 13);
+            this.labelSenha.TabIndex = 3;
+            this.labelSenha.Text = "Senha";
+            // 
+            // ColetaUserESenhaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ColetaUserESenhaForm";
+            this.ClientSize = new System.Drawing.Size(222, 144);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.labelUsuario);
+            this.Name = "ColetaUserESenhaForm";
+            this.Text = "loginOTRS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Label labelSenha;
     }
 }
--- ConsoleApp1/ConsoleApp1/ColetaUserESenhaForm.cs
+++ ConsoleApp1/ConsoleApp1/ColetaUserESenhaForm.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ConsoleApp1
 {
     public partial class ColetaUserESenhaForm : Form
     {
+
         public ColetaUserESenhaForm()
         {
             InitializeComponent();
+            InitializeMyControl();
+        }
+
+        public void buttonLogin_Click(object sender, EventArgs e)
+        {
+            
+            Close();
+        }
+        private void InitializeMyControl()
+        {
+            textBoxSenha.PasswordChar = '*';
         }
     }
 }
