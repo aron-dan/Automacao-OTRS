f25289d07ee24b402edc20e7a24f8a5ccdc035a7
aron-dan <82916233+aron-dan@users.noreply.github.com>
2023-06-26 21:47:11 -0300

    feat: Adicionado tryCath para tentativa de login juntamente com form de erro

--- ConsoleApp1/ConsoleApp1/AutomationWeb.cs
+++ ConsoleApp1/ConsoleApp1/AutomationWeb.cs
@@ -11,20 +11,32 @@ namespace ConsoleApp1
     public class AutomationWeb
     {
         ColetaDadosUsuario coletaDadosUsuario = new ColetaDadosUsuario();
+        ErroForm erroForm = new ErroForm();
         Program program = new Program();
         public IWebDriver driver;
 
 
         public void login()
         {
+
+            try
+            {
+                string email = coletaDadosUsuario.RetornaEmail();
+                string senha = coletaDadosUsuario.RetornaSenha();
+                driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://atende.sempre.net.br/portal/index.html?RequestedURL=Action%3DAgentTicketEmail");
+                driver.FindElement(By.Id("User")).SendKeys(email);
+                driver.FindElement(By.Id("Password")).SendKeys(senha);
+                driver.FindElement(By.Id("LoginButton")).Click();
+                driver.FindElement(By.Id("Fulltext")).Click();
+            }
+            catch (Exception)
+            {
+
+                erroForm.ShowDialog();
+            }
             
-            string email = coletaDadosUsuario.RetornaEmail();
-            string senha = coletaDadosUsuario.RetornaSenha();
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://atende.sempre.net.br/portal/index.html?RequestedURL=Action%3DAgentTicketEmail");
-            driver.FindElement(By.Id("User")).SendKeys(email);
-            driver.FindElement(By.Id("Password")).SendKeys(senha);
-            driver.FindElement(By.Id("LoginButton")).Click();
+
         }
     
     
